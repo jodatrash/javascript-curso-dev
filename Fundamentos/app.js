b480eec7e68a5ee2f0e1914d3763f2db24f92738
Jonathan Pacheco Cruz <JonathanPCruz.dev@hotmail.com>
2026-01-19 09:25:36 -0600

    feat: Se añade app.js

---
+++ Fundamentos/app.js
@@ -0,0 +1,11 @@
+// console.log('Hola Mundo por medio de app.js');
+// document.writeln('Hola Mundo x2');
+
+let a = 10;
+var b = 10;
+const c = 10;
+
+// c = 20; No se puede hacer
+
+a = 20;
+b = 30;
